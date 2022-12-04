14e1d1ed1b60942d1ab1502bf923cb409c4e8c36
Anastasia <an.afalin@yandex.ru>
2022-12-04 20:20:08 +0300

    Update solution to p.3 of task on topic Spring. MVC

--- spring-mvc/src/main/java/ru/lazarenko/web/controller/PersonController.java
+++ spring-mvc/src/main/java/ru/lazarenko/web/controller/PersonController.java
@@ -11,17 +11,18 @@ import ru.lazarenko.web.model.Person;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Controller
 public class PersonController {
-    private static List<Person> LIST_PERSONS = new ArrayList<>();
+    private static List<Person> LIST_PEOPLE = new ArrayList<>();
 
     static {
-        LIST_PERSONS.add(new Person("Ana", 25, "female"));
-        LIST_PERSONS.add(new Person("Slava", 45, "male"));
-        LIST_PERSONS.add(new Person("Slavik", 35, "male"));
-        LIST_PERSONS.add(new Person("Irina", 42, "female"));
-        LIST_PERSONS.add(new Person("Nikolai", 22, "male"));
+        LIST_PEOPLE.add(new Person("Ana", 25, "female"));
+        LIST_PEOPLE.add(new Person("Slava", 45, "male"));
+        LIST_PEOPLE.add(new Person("Slavik", 35, "male"));
+        LIST_PEOPLE.add(new Person("Irina", 42, "female"));
+        LIST_PEOPLE.add(new Person("Nikolai", 22, "male"));
     }
 
     @GetMapping("/")
@@ -29,18 +30,18 @@ public class PersonController {
         return "start";
     }
 
-    @GetMapping("/postInput")
+    @GetMapping("/post-input")
     public String getPostInputPage(Model model) {
         model.addAttribute("person", new Person());
         return "form-post-request";
     }
 
-    @GetMapping("/getInput")
+    @GetMapping("/get-input")
     public String getGetInputPage() {
         return "form-get-request";
     }
 
-    @GetMapping("/deletePerson")
+    @GetMapping("/delete-person")
     public String getDeletePersonPage() {
         return "form-delete";
     }
@@ -51,21 +52,21 @@ public class PersonController {
                                   @RequestParam String gender,
                                   Model model) {
         Person person = new Person(name, age, gender);
-        LIST_PERSONS.add(person);
+        LIST_PEOPLE.add(person);
         model.addAttribute("person", person);
         return "form-result-input";
     }
 
     @PostMapping("/form-handler")
     public String createPersonPost(@ModelAttribute Person person) {
-        LIST_PERSONS.add(person);
-        return "redirect:/resInput";
+        LIST_PEOPLE.add(person);
+        return "redirect:/res-input";
     }
 
-    @GetMapping("/resInput")
+    @GetMapping("/res-input")
     public String getInputResult(Model model) {
-        model.addAttribute("persons", LIST_PERSONS);
-        return "form-list-person";
+        model.addAttribute("people", LIST_PEOPLE);
+        return "form-list-people";
     }
 
     @GetMapping("/create")  // /create?name=Ana&age=25&gender=female
@@ -74,85 +75,64 @@ public class PersonController {
                                            @RequestParam String gender,
                                            Model model) {
         Person person = new Person(name, age, gender);
-        LIST_PERSONS.add(person);
+        LIST_PEOPLE.add(person);
         model.addAttribute("name", name);
         model.addAttribute("age", age);
         model.addAttribute("gender", gender);
         return "person";
     }
 
-    @GetMapping("/deletePersonByValue")
+    @GetMapping("/delete-person-by-value")
     public String getPageAfterDelete(@RequestParam String type,
                                      @RequestParam String value,
                                      Model model) {
+        Stream<Person> stream = LIST_PEOPLE.stream();
+
         if (type.equals("age")) {
-            LIST_PERSONS = LIST_PERSONS
-                    .stream()
-                    .filter(person -> person.getAge() != Integer.parseInt(value))
-                    .collect(Collectors.toList());
+            stream = stream.filter(person -> person.getAge() != Integer.parseInt(value));
         } else if (type.equals("name")) {
-            LIST_PERSONS = LIST_PERSONS
-                    .stream()
-                    .filter(person -> !person.getName().equalsIgnoreCase(value))
-                    .collect(Collectors.toList());
+            stream = stream.filter(person -> !person.getName().equalsIgnoreCase(value));
         } else {
-            LIST_PERSONS = LIST_PERSONS
-                    .stream()
-                    .filter(person -> !person.getGender().equalsIgnoreCase(value))
-                    .collect(Collectors.toList());
+            stream = stream.filter(person -> !person.getGender().equalsIgnoreCase(value));
         }
-        model.addAttribute("persons", LIST_PERSONS);
-        return "form-list-person";
+        LIST_PEOPLE = stream.collect(Collectors.toList());
+        model.addAttribute("people", LIST_PEOPLE);
+        return "form-list-people";
     }
 
-    @GetMapping("/sortList")
+    @GetMapping("/sort-list")
     public String getPageSortPerson() {
-        return "form-sort-list-person";
+        return "form-sort-list-people";
     }
 
-    @GetMapping("/showSortPerson")
+    @GetMapping("/show-sort-person")
     public String getPageAfterDelete(@RequestParam String count,
                                      @RequestParam String sortBy,
                                      @RequestParam String sortType,
                                      Model model) {
+        Stream<Person> stream = LIST_PEOPLE.stream();
+
         if (sortBy.equals("name")) {
             if (sortType.equals("ascending")) {
-                model.addAttribute("persons", LIST_PERSONS
-                        .stream()
-                        .sorted((p1, p2) -> p1.getName().compareTo(p2.getName()))
-                        .limit(Integer.parseInt(count)));
+                stream = stream.sorted((p1, p2) -> p1.getName().compareTo(p2.getName()));
             } else {
-                model.addAttribute("persons", LIST_PERSONS
-                        .stream()
-                        .sorted((p1, p2) -> p2.getName().compareTo(p1.getName()))
-                        .limit(Integer.parseInt(count)));
+                stream = stream.sorted((p1, p2) -> p2.getName().compareTo(p1.getName()));
             }
-
         } else if (sortBy.equals("age")) {
             if (sortType.equals("ascending")) {
-                model.addAttribute("persons", LIST_PERSONS
-                        .stream()
-                        .sorted((p1, p2) -> p1.getAge().compareTo(p2.getAge()))
-                        .limit(Integer.parseInt(count)));
+                stream = stream.sorted((p1, p2) -> p1.getAge().compareTo(p2.getAge()));
             } else {
-                model.addAttribute("persons", LIST_PERSONS
-                        .stream()
-                        .sorted((p1, p2) -> p2.getAge().compareTo(p1.getAge()))
-                        .limit(Integer.parseInt(count)));
+                stream = stream.sorted((p1, p2) -> p2.getAge().compareTo(p1.getAge()));
             }
         } else {
             if (sortType.equals("male")) {
-                model.addAttribute("persons", LIST_PERSONS
-                        .stream()
-                        .sorted((p1, p2) -> p2.getGender().compareTo(p1.getGender()))
-                        .limit(Integer.parseInt(count)));
+                stream = stream.sorted((p1, p2) -> p2.getGender().compareTo(p1.getGender()));
             } else {
-                model.addAttribute("persons", LIST_PERSONS
-                        .stream()
-                        .sorted((p1, p2) -> p1.getGender().compareTo(p2.getGender()))
-                        .limit(Integer.parseInt(count)));
+                stream = stream.sorted((p1, p2) -> p1.getGender().compareTo(p2.getGender()));
             }
         }
-        return "form-list-person";
+
+        model.addAttribute("people", stream.limit(Integer.parseInt(count)).collect(Collectors.toList()));
+        return "form-list-people";
     }
 }
